d80c8cff7329725d95fc8a5687feb56608d78b08
safiq lodi <safiqlodi2@gmail.com>
2021-04-04 12:12:28 +0530

    added more routes for crud operation

---
+++ models/article.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAT: {
+        type: Date,
+        default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+articleSchema.pre('validate', function(next){
+    console.log("THUSSS", this)
+    if(this.title){
+        this.slug = slugify(this.title, {
+            lower: true, strict: true
+        })
+    }
+    next()
+})
+
+module.exports = mongoose.model('Article', articleSchema)
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost:27017/blog',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/articles',articleRouter)
 
-app.get('/', (req, res)=>{
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test'
-    },
-        {
-            title: 'Test Article 2',
-            createdAt: new Date(),
-            description: 'Test'
-        }]
+app.get('/', async (req, res)=>{
+    const articles = await Article.find().sort({createdAt: 'desc'})
     res.render('articles/index', {articles: articles})
 })
 
